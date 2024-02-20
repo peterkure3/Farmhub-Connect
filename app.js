58723d5266efc541c107413edaecd46cc1b48f5e
Peter Kure <peterkure256@gmail.com>
2024-02-20 18:45:08 +0300

    Update: Route for serving html

--- app.js
+++ app.js
@@ -2,14 +2,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const productRouter = require('./Routes/items');
-
 const userRouter = require('./Routes/user');
 const cors = require('cors');
+const path = require('path'); // Import the path module
 const app = express();
 const port = 3000;
 
 app.use(cors());
-
 app.use(bodyParser.json());
 
 app.use('/api', productRouter);
@@ -19,6 +18,11 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+// Serve static files from the 'agro_sell' folder
+app.use(express.static(path.join(__dirname, 'agro sell')));
+
+// Route for serving the index.html file
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  // Send the index.html file located in the 'agro_sell' folder
+  res.sendFile(path.join(__dirname, 'agro sell', 'landing_page.html'));
 });
